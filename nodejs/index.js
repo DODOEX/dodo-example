0024a63f232d17b047fcd775e11e36b0817bd221
sa1i <sai@dodoex.io>
2021-12-02 03:58:28 +0800

    add nodejs project

---
+++ nodejs/index.js
@@ -0,0 +1,139 @@
+const axios = require("axios").default;
+const { ethers } = require("ethers");
+const erc20ABI = require("./erc20.json");
+
+const privateKey = env.YOUR_PK;
+// please remember keep your wallet private key safe and split it from source code repo in your project
+// this is just for demo usage.
+const rpcUrl = "https://bsc-dataseed.binance.org";
+
+const rpcProvider = new ethers.providers.JsonRpcProvider(rpcUrl);
+
+const wallet = new ethers.Wallet(privateKey, rpcProvider);
+
+const dodoAPI = "https://route-api.dodoex.io/dodoapi/getdodoroute";
+
+const checkAllowance = async (
+  tokenAddress,
+  targetAddress,
+  userAddress,
+  fromAmount
+) => {
+  const erc20Contract = new ethers.Contract(
+    tokenAddress,
+    erc20ABI,
+    rpcProvider
+  );
+  const allowance = await erc20Contract.allowance(userAddress, targetAddress);
+  console.log(
+    "allowance > fromAmount => ",
+    allowance.toString(),
+    fromAmount,
+    allowance.gt(`${fromAmount}`)
+  );
+  return allowance.gt(`${fromAmount}`);
+};
+
+const doApprove = async (
+  tokenAddress,
+  targetAddress,
+  userAddress,
+  fromAmount
+) => {
+  const erc20Contract = new ethers.Contract(
+    tokenAddress,
+    erc20ABI,
+    rpcProvider
+  );
+  await erc20Contract.approve(targetAddress, fromAmount);
+};
+
+const doSwap = async (txObj) => {
+  const result = await wallet.sendTransaction(txObj);
+  console.log(
+    "txHash => ",
+    result,
+    `\nopen https://bscscan.com/tx/${result.hash} check result.`
+  );
+  return result;
+};
+
+const testFlight = () => {
+  const fromTokenAddress = "0x67ee3Cb086F8a16f34beE3ca72FAD36F7Db929e2";
+  const toTokenAddress = "0x55d398326f99059ff775485246999027b3197955";
+  const fromAmount = 0.3 * 1e18;
+  axios
+    .get(dodoAPI, {
+      params: {
+        // DODO
+        fromTokenAddress: fromTokenAddress,
+        fromTokenDecimals: 18,
+        // USDT
+        toTokenAddress: toTokenAddress,
+        toTokenDecimals: 6,
+        // amount with decimal
+        fromAmount: fromAmount,
+        slippage: 1,
+        userAddr: wallet.address,
+        // BSC chain id is 56
+        chainId: 56,
+        rpc: rpcUrl,
+      },
+    })
+    .then(async function (response) {
+      console.log("response data => ", response.data);
+      if (response.data.status === 200) {
+        const routeObj = response.data.data;
+        // check allowance first
+        const targetAddress = routeObj.targetApproveAddr;
+        // allowance should greater than fromAmount
+        const hasApproved = await checkAllowance(
+          fromTokenAddress,
+          targetAddress,
+          wallet.address,
+          fromAmount
+        );
+
+        if (!hasApproved) {
+          await doApprove(
+            fromTokenAddress,
+            targetAddress,
+            wallet.address,
+            fromAmount
+          );
+        }
+
+        const gasLimit = await wallet.estimateGas({
+          to: routeObj.to,
+          data: routeObj.data,
+          value: 0, // if fromToken is eth or bnb or ht, value should be fromAmount
+        });
+        console.log("gasLimit => ", gasLimit);
+
+        const gasPrice = await wallet.getGasPrice();
+        console.log("gasPrice => ", gasPrice);
+
+        const nonce = await wallet.getTransactionCount();
+        console.log("nonce => ", nonce);
+
+        const tx = {
+          from: wallet.address,
+          to: routeObj.to,
+          value: 0, // if fromToken is eth or bnb or ht, value should be fromAmount
+          nonce: nonce,
+          gasLimit: ethers.utils.hexlify(gasLimit),
+          gasPrice: ethers.utils.hexlify(gasPrice),
+        };
+
+        await doSwap(tx);
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    .then(function () {
+      console.log("Swap Done.");
+    });
+};
+
+testFlight();
